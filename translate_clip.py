2ec745370ac24e78f898e046f612acbbf9b916c7
Daniel Calderon <danielisaias460@gmail.com>
2023-04-18 12:09:20 -0700

    The necessary modules were added for the script to work, and a test was carried out to verify its correct operation

--- translate_clip.py
+++ translate_clip.py
@@ -1,13 +1,22 @@
-#This is a test
-#This is the command the you need to obtein the translate text 
-#trans -no-ansi -brief  "hola mundo" (automaticamente lo manda a ingles) 
+
+import sys
+#This module allows us to obtain the parameters from the script
 
 import subprocess  
+#This modulo allows us to execute commands in the shell
 
 #In this variable we store the spanish text to translate
-text = "" 
+text = sys.argv[1]
 
 
-#In this variable we store the output of the translation 
-english_trans = subprocess.check_output("trans -no-ansi -brief "+ , shell=True)
+# "trans" is the alias of the (Translate-shell) tool
+# "trans -no-ansi -brief text-to-translate" is the command to get the simplified translation without any other information
+#In this variables we store the output of the translation
+english_trans_bytes = subprocess.check_output('trans -no-ansi -brief "{}"'.format(text), shell=True)
+english_trans_text = english_trans_bytes.decode('utf-8')
+ 
+
+#We print the result to verify that the translation is correct
+print(english_trans_text)
+
 
